ec1858c5c1326ae83443819719041274bfb20010
Alex Tsvetanov <alex.tsvetanov0+github@gmail.com>
2025-04-04 23:20:15 +0300

    Second window

---
+++ Invoices/View/AlternativeMainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using InvoiceApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InvoiceApp.View
+{
+    /// <summary>
+    /// Interaction logic for SecondaryWindow.xaml
+    /// </summary>
+    public partial class AlternativeMainWindow : Window
+    {
+        public AlternativeMainWindow(MainWindowViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+}
---
+++ sources/Invoice.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace InvoiceApp.Database;
+
+public class Invoice : ObservableObject
+{
+    // DB columns
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Number { get; set; }
+    public DateTime Date { get; set; }
+    private int CustomerId { get; set; }
+    private string InvoiceCurrencyName { get; set; }
+
+    // Mapped properties
+    [ForeignKey("CustomerId")] 
+    public Customer Customer { get; set; }
+    public ICollection<InvoiceLine> InvoiceLines { get; set; }
+    public Type InvoiceCurrency
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(InvoiceCurrencyName))
+            {
+                return Type.GetType(InvoiceCurrencyName);
+            }
+            return null;
+        }
+        set
+        {
+            InvoiceCurrencyName = value?.FullName;
+        }
+    }
+
+    public class Mapper : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+            builder.Property(x => x.Number).IsRequired();
+            builder.Property(x => x.Date).IsRequired();
+            builder.HasOne(x => x.Customer).WithMany().HasForeignKey(x => x.CustomerId).IsRequired();
+            builder.Property(x => x.InvoiceCurrencyName).IsRequired();
+            builder.Ignore(x => x.InvoiceCurrency);
+        }
+    }
+}
